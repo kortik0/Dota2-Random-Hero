a971f564d5851875eaa9ceb8ceb1a2719b0c76e6
kortik0 <46229206+kortik0@users.noreply.github.com>
2022-01-25 21:55:41 +0300

    Update _document.js
    Add meta tags for robot parser. 
    Probably will delete later or change to analytic systems, but much later

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ export default class ThisAppDocument extends Document {
         <Head>
           <title>Hero Randomer</title>
           <meta name={"description"} />
+          <meta name="google-site-verification" content="VCHFDWGu5MM7TifIFMI_RvW1eeCpW6IWTMaW1T5uXIk" />
+          <meta name="yandex-verification" content="bc1861abcb280b2e" />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito&display=swap"
             rel="stylesheet"
