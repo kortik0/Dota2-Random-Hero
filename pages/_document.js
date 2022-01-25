b7250e2eb5a572e04e7ee9fd0d2b9650875b3471
kortik0 <46229206+kortik0@users.noreply.github.com>
2022-01-25 21:58:48 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class ThisAppDocument extends Document {
           <title>Hero Randomer</title>
           <meta name={"description"} />
           <meta name="google-site-verification" content="VCHFDWGu5MM7TifIFMI_RvW1eeCpW6IWTMaW1T5uXIk" />
-          <meta name="yandex-verification" content="bc1861abcb280b2e" />
+          <meta name="yandex-verification" content="d09860e19ea9c05b" />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito&display=swap"
             rel="stylesheet"
